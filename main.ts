cdd9b0e5e8fb0d63982f5779ab45eee216dd7a6e
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2021-01-21 19:10:33 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+while (true) {
+    console.log(input.soundLevel() + input.lightLevel())
+    while (input.lightLevel() < 6) {
+        if (input.soundLevel() > 100) {
+            light.setAll(color.rgb(255, 0, 0))
+            music.siren.playUntilDone()
+        }
+        
+    }
+}
